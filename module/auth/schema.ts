b492d787c01e9634141762a89e65e7b048e7039b
Harshit Garg <gargh2120@gmail.com>
2026-03-22 23:57:19 +0530

    validation here

--- module/auth/schema.ts
+++ module/auth/schema.ts
@@ -1,11 +1,11 @@
-import { email, z } from "zod";
+import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.email(),
   password: z.string().min(6),
 });
 export const signupSchema = z.object({
   name: z.string().min(2),
-  email: z.string().email(),
+  email: z.email(),
   password: z.string().min(6),
 });
