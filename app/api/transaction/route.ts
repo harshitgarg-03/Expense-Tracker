e3e288362ff0e277f9cd3bffe86c2eeb1f95c2b2
Harshit Garg <gargh2120@gmail.com>
2026-03-23 22:31:23 +0530

    transaction routes defined

---
+++ app/api/transaction/route.ts
@@ -0,0 +1,37 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET() {
+  const session = await auth.api.getSession({ headers: headers() });
+
+  if (!session?.user) {
+    return new Response("unauthorized access ", { status: 401 });
+  }
+
+  const transaction = await prisma.transaction.findMnay({
+    where: {
+      userId: session.user.id,
+    },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return Response.json(transaction);
+}
+
+export async function POST(req: Request) {
+  const session = await auth.api.getSession({ headers: headers() });
+
+  if (!session?.user) {
+    return new Response("unauthorized user ", { status: 401 });
+  }
+
+  const data = await req.json();
+  const transaction = await prisma.transaction.create({
+    data: {
+      ...data,
+      userId: session.user.id,
+    },
+  });
+  return Response.json(transaction);
+}
