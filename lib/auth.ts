f5877876708d591539bba58701bbb620ef73dfd1
Harshit Garg <gargh2120@gmail.com>
2026-03-23 12:41:25 +0530

    fix the current user cookies and sessions authenticate

--- lib/auth.ts
+++ lib/auth.ts
@@ -11,10 +11,25 @@ export const auth = betterAuth({
     enabled: true,
   },
 
-  socialProviders: { 
-    github: { 
-      clientId: process.env.GITHUB_CLIENT_ID as string, 
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
-    }}
-}
-);
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+  },
+
+  cookies: {
+    sessionToken: {
+      name: "session_token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+      },
+    },
+  },
+});
