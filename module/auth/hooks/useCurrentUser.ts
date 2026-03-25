23acf38869bb49f4eadf4185d0fa2b069efc39b3
Harshit Garg <gargh2120@gmail.com>
2026-03-25 11:02:54 +0530

    auth hooks fix error

--- module/auth/hooks/useCurrentUser.ts
+++ module/auth/hooks/useCurrentUser.ts
@@ -4,18 +4,30 @@ import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "../services/auth.api";
 import { useAuthStore } from "../store";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 export const useCurrentUser = () => {
+    const isAuthenticate = false;
+    const userId = "";
     const setUser = useAuthStore(s => s.setUser);
     const router = useRouter();
-    return useQuery({
+    const {data} =  useQuery({
         queryKey: ["current-user"],
         queryFn: async () => {
             const data = await getCurrentUser();
             if(data?.user) setUser(data.user);
-            // rou
             return data;
         },
     });
+
+    useEffect(() => {
+        if (data) {
+            setUser(data.user);
+            isAuthenticate: true;
+            userId: data.user._id
+            router.push("/dashboard");
+        }
+    }, [data, setUser])
+    return data;
 };
