ddb0e3579d4eca8fba2cfc2875b789d54be4aa4a
Harshit Garg <gargh2120@gmail.com>
2026-03-31 20:59:00 +0530

    defiend layout and home

---
+++ app/(root)/home/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import HomePage from '@/module/home/components/home/homepage'
+
+function page() {
+  return (
+    <HomePage/>
+  )
+}
+
+export default page
---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { DashboardPage } from '@/module/home/components/dashboard/dashboardpage'
+import HomePage from '@/module/home/components/home/homepage'
+
+function page() {
+  return (
+    <div>
+        <DashboardPage/>
+        <HomePage/>
+    </div>
+  )
+}
+
+export default page
