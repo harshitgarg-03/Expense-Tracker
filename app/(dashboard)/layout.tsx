737d1a4cabd10c2be60f2f3833088c09018d787c
Harshit Garg <gargh2120@gmail.com>
2026-04-03 09:21:50 +0530

    fixing padding

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { Sidebar } from "@/components/sidebar";
 
+
 export default async function DashboardLayout({ children }: {children: React.ReactNode}) {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -13,13 +14,13 @@ export default async function DashboardLayout({ children }: {children: React.Rea
   }
   
   return (
-  <div className="p-4 flex h-auto w-full gap-5 overflow-hidden">
+  <div className="p-4 flex h-screen w-full gap-5 overflow-hidden">
     
     {/* Sidebar */}
     <Sidebar />
 
     {/* Content */}
-    <main className="flex-1 h-full overflow-hidden">
+    <main className="flex-1 h-full overflow-y-auto no-scrollbar">
       {children}
     </main>
 
