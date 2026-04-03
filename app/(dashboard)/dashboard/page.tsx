fdaec6d3d1c5d45efeae88c4cec5dbc4b907332d
Harshit Garg <gargh2120@gmail.com>
2026-04-03 09:23:16 +0530

    dashboard page

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { DashboardPage } from '@/module/home/components/dashboard/dashboardpage'
+
+function page() {
+  return (
+    <DashboardPage/> 
+  )
+}                
+
+export default page
