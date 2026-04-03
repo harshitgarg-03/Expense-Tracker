9fb510acc7ae785042079880162702d757e12c54
Harshit Garg <gargh2120@gmail.com>
2026-04-03 09:05:13 +0530

    fixing padding

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,28 @@
-import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { Sidebar } from "@/components/sidebar";
 
 export default async function DashboardLayout({ children }: {children: React.ReactNode}) {
-  const session = await auth.api.getSession();
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  }); 
 
-  if (!session) {
+  if(!session?.user){
     redirect("/login");
   }
+  
+  return (
+  <div className="p-4 flex h-auto w-full gap-5 overflow-hidden">
+    
+    {/* Sidebar */}
+    <Sidebar />
 
-  return <>{children}</>;
+    {/* Content */}
+    <main className="flex-1 h-full overflow-hidden">
+      {children}
+    </main>
+
+  </div>
+);
 }
