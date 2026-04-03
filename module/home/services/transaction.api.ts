deab3e3c9bc3212fb64a7a9c9ce87cd2b68503fb
Harshit Garg <gargh2120@gmail.com>
2026-04-03 11:08:10 +0530

    add txns apis

---
+++ module/home/services/transaction.api.ts
@@ -0,0 +1,41 @@
+import { Transaction } from "../types";
+
+export const getTransactions = async (): Promise<Transaction[]> => {
+  try {
+    const res = await fetch("/api/transaction");
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch transactions");
+    }
+
+    const data: Transaction[] = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching transactions:", error);
+    return [];
+  }
+};
+
+export const postTransaction = async (
+  payload: Transaction,
+): Promise<Transaction> => {
+  try {
+    const res = await fetch("/api/transaction", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to create transaction");
+    }
+
+    const result: Transaction = await res.json();
+    return result;
+  } catch (error) {
+    console.error("PostTransaction Error:", error);
+    throw error;
+  }
+};
