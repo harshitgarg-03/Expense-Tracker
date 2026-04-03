0fb2a4e8d558593845834d23a708a5029e43e6f5
Harshit Garg <gargh2120@gmail.com>
2026-04-03 11:16:25 +0530

    auth store defined

--- module/auth/store.ts
+++ module/auth/store.ts
@@ -1,7 +1,11 @@
 import { create } from "zustand";
 
-export const useAuthStore = create<useAuthprop>((set) => ({
+export const useAuthStore = create<useAuthprop>((set, get) => ({
   user: null,
   setUser: (user: authProp) => set({ user }),
-  logout: () => set({ user: null }),
+  logout: () => {
+    
+    set({ user: null })},
+    
+    
 }));
