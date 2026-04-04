95188faa7454c05800ab13da907d4ceeb7982e0e
Harshit Garg <gargh2120@gmail.com>
2026-04-04 21:09:11 +0530

    hooks defined

--- module/home/hooks/useAddTransaction.ts
+++ module/home/hooks/useAddTransaction.ts
@@ -1,19 +1,24 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postTransaction } from "../services/transaction.api";
 import { toast } from "sonner";
+import { useForm } from 'react-hook-form';
 
 export const useAddTransaction = () => {
   const queryClient = useQueryClient();
-
+  const { reset } = useForm();
   return useMutation({
     mutationFn: postTransaction,
 
     onSuccess: () => {
+      console.log("succs txn");
+      
       queryClient.invalidateQueries({ queryKey: ["transaction"] });
-      toast.success("Transaction success! 👍")
+      toast.success("Transaction success! 👍");
+      reset();
     },
     onError: () => {
-      toast.success("Transaction failed! 👎")
+      console.error("error txn");
+      toast.error("Transaction failed! 👎")
     }
   });
 };
