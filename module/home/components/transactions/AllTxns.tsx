8b5882280c02e8143fcb3eb17a631bd6576689d8
Harshit Garg <gargh2120@gmail.com>
2026-04-09 10:03:59 +0530

    transaction apis defimed and fixes

--- module/home/components/transactions/AllTxns.tsx
+++ module/home/components/transactions/AllTxns.tsx
@@ -1,4 +1,5 @@
-import { Pencil, Trash2 } from 'lucide-react';
+"use client"
+import { Pencil, Trash2 } from "lucide-react";
 import {
   Table,
   TableBody,
@@ -6,20 +7,23 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table';
-import { Button } from '@/components/ui/button';
-import { Badge } from '@/components/ui/badge';
-import { format } from 'date-fns';
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { format } from "date-fns";
+import { useTransaction } from "../../hooks/useTransaction";
+import { CATEGORY_COLORS, Transaction } from "../../types";
 
-export function TransactionTableUI({ transactions }: any) {
+export function TransactionTableUI() {
+  const { balance, monthlyData, transactions, isLoading } = useTransaction();
+  
   return (
     <div className="space-y-4">
-      
       {/* Header */}
       <div className="flex justify-between items-center">
         <h2 className="text-lg font-semibold">Transactions</h2>
         <span className="text-sm text-gray-500">
-          {transactions.length}  items
+          {transactions.length} items
         </span>
       </div>
 
@@ -40,40 +44,42 @@ export function TransactionTableUI({ transactions }: any) {
           <TableBody>
             {transactions.length === 0 ? (
               <TableRow>
-                <TableCell colSpan={6} className="text-center py-8 text-gray-500">
+                <TableCell
+                  colSpan={6}
+                  className="text-center py-8 text-gray-500"
+                >
                   No transactions found
                 </TableCell>
               </TableRow>
             ) : (
-              transactions.map((tx: any) => {
-                const isIncome = tx.type === 'income';
-
+              transactions.map((tx: Transaction) => {
+                const isIncome = tx.type === "INCOME";
+                const categoryConfig = CATEGORY_COLORS[tx.category];
+                
                 return (
                   <TableRow key={tx.id}>
-                    <TableCell className="font-medium">
-                      {tx.title}
+                    <TableCell className="font-medium">{tx.title}</TableCell>
+
+                    <TableCell>
+                      <Badge className={`${categoryConfig.bgColor} ${categoryConfig.color}`} >{tx.category}</Badge>
                     </TableCell>
 
                     <TableCell>
-                      <Badge>{tx.category}</Badge>
-                    </TableCell>
-
-                    <TableCell>
-                      <Badge variant={isIncome ? 'default' : 'secondary'}>
+                      <Badge variant={isIncome ? "default" : "secondary"}>
                         {tx.type}
                       </Badge>
                     </TableCell>
 
                     <TableCell>
-                      {format(new Date(tx.date), 'MMM dd, yyyy')}
+                      {format(new Date(tx.date), "MMM dd, yyyy")}
                     </TableCell>
 
                     <TableCell
                       className={`font-semibold ${
-                        isIncome ? 'text-green-600' : 'text-red-600'
+                        isIncome ? "text-green-600" : "text-red-600"
                       }`}
                     >
-                      {isIncome ? '+' : '-'}${tx.amount}
+                      {isIncome ? "+" : "-"}${tx.amount}
                     </TableCell>
 
                     <TableCell className="text-right">
