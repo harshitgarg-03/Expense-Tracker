0353d386cbbdb58e475898bf213dcd326e4d9ccb
Harshit Garg <gargh2120@gmail.com>
2026-04-12 19:40:19 +0530

    adding usedelete hook

--- module/home/hooks/useAddTransaction.ts
+++ module/home/hooks/useAddTransaction.ts
@@ -6,20 +6,17 @@ import { useForm } from 'react-hook-form';
 export const useAddTransaction = () => {
   const queryClient = useQueryClient();
   const { reset } = useForm();
-  // console.log("use add transcation ");
                 
   return useMutation({ 
     mutationFn: postTransaction,
 
     onSuccess: () => {
-      // console.log("succs txn");
       
       queryClient.invalidateQueries({ queryKey: ["transaction"] });
       toast.success("Transaction success! 👍");
       reset();
     },
     onError: () => {
-      // console.error("error txn");
       toast.error("Transaction failed! 👎")
     }
   });
---
+++ module/home/hooks/useDeleteTransaction.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteTransaction } from "../services/transaction.api";
+import { toast } from "sonner";
+
+export const useDeleteTransaction = () => {
+  const queryclient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteTransaction,
+
+    onSuccess: () => {
+      queryclient.invalidateQueries({ queryKey: ["transaction"] });
+      toast.success("Transaction Delete success! 👍");
+    },
+    onError: () => {
+      toast.error("Transaction Delete failed! 👎");
+    },
+  });
+};
