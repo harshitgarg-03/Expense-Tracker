dd82b68e0f8108d5b25c5a36c66173aa027de1cb
Harshit Garg <gargh2120@gmail.com>
2026-04-15 12:15:16 +0530

    fix css sun and moon

--- module/home/components/dashboard/header.tsx
+++ module/home/components/dashboard/header.tsx
@@ -1,11 +1,11 @@
-import { Moon, Sun } from 'lucide-react';
-import { useTheme } from 'next-themes';
-import { Button } from '@/components/ui/button';
-import { HeaderProps } from '../../types';
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { HeaderProps } from "../../types";
 
 export function Header({ title, subtitle }: HeaderProps) {
   const { theme, setTheme } = useTheme();
-  
+
   return (
     <div className="flex items-center justify-between mb-8">
       <div>
@@ -19,11 +19,14 @@ export function Header({ title, subtitle }: HeaderProps) {
         size="icon"
         onClick={() => {
           console.log("hii theme ");
-          
-          setTheme(theme === 'dark' ? 'light' : 'dark')}}
+
+          setTheme(theme === "dark" ? "light" : "dark");
+        }}
       >
-        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <div className="relative h-5 w-5" >
+        <Sun className="h-5 w-5 absolute transition-all duration-300 rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
+        <Moon className="h-5 w-5 absolute transition-all duration-300 rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
+        </div>
         <span className="sr-only">Toggle Theme</span>
       </Button>
     </div>
