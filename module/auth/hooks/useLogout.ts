23cf150e22ac35fc98dce6dd2822131c6f681781
Harshit Garg <gargh2120@gmail.com>
2026-04-15 11:38:03 +0530

    import toast

--- module/auth/hooks/useLogout.ts
+++ module/auth/hooks/useLogout.ts
@@ -2,6 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useAuthStore } from "../store";
 import { useRouter } from "next/navigation";
 import { logoutUser } from "../services/action.api";
+import { toast } from "sonner";
 
 export const useLogout = () => {
     const logout = useAuthStore(s => s.logout);
@@ -11,7 +12,14 @@ export const useLogout = () => {
         mutationFn: logoutUser,
         onSuccess: () => {
             logout();
+            toast.success("Logout success 🎉🎉")
             navigate.push("/home")
         },
+
+        onError: (error) => {
+            toast.error(error.message || "Logout failed 👎", {
+        className: "bg-red-600 text-white"
+      })
+        }
     });
 };
