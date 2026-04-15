eaccd57313f0f70e355fdbe54f8491229492436e
Harshit Garg <gargh2120@gmail.com>
2026-04-15 21:35:11 +0530

    fix error and remove unnecessary imports

--- module/home/hooks/useAddTransaction.ts
+++ module/home/hooks/useAddTransaction.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postTransaction } from "../services/transaction.api";
 import { toast } from "sonner";
-import { useForm } from 'react-hook-form';
+
 
 export const useAddTransaction = () => {
   const queryClient = useQueryClient();
