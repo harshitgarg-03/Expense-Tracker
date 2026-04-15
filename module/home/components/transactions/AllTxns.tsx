0e6760f511548fcb4ff10fd4503bdde907dff849
Harshit Garg <gargh2120@gmail.com>
2026-04-15 23:08:06 +0530

    adding extra functionality

--- module/home/components/transactions/AllTxns.tsx
+++ module/home/components/transactions/AllTxns.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { Pencil, Trash2 } from "lucide-react";
@@ -21,20 +20,35 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { categories } from "../addExpense/addExpensePage";
+import { categories, ExpenseFormUI } from "../addExpense/addExpensePage";
 import { Label } from "@/components/ui/label";
 import { useDeleteTransaction } from "../../hooks/useDeleteTransaction";
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 export function TransactionTableUI() {
   const { transactions, isLoading } = useTransaction();
   const { mutate } = useDeleteTransaction();
-
+  const [open, setopen] = useState<boolean>(false);
   const handledelete = (id: string) => {
     mutate(id);
-  }
+  };
+  const handleEdit = (id: string) => {
+    setopen(!open);
+  };
   return (
     <div className="space-y-6">
+      {/* {open &&  */}
+        <Dialog open={open}  onOpenChange={setopen} >
+          <DialogTrigger>
+            <button>Edit Transaction</button>
+          </DialogTrigger>
 
+          <DialogContent className="w-7xl">
+            <ExpenseFormUI mode="Edit" />
+          </DialogContent>
+        </Dialog>
+      {/* } */}
       {/* Header */}
       <div className="space-y-1">
         <h1 className="text-2xl font-semibold">Transactions</h1>
@@ -45,40 +59,38 @@ export function TransactionTableUI() {
 
       {/* Filter + Count */}
       <div className="flex items-end justify-between gap-4 flex-wrap">
+        {/* Left: Category Filter */}
+        <div className="flex flex-col">
+          <Label className="text-xs text-gray-500 mb-1">Category</Label>
 
-  {/* Left: Category Filter */}
-  <div className="flex flex-col">
-    <Label className="text-xs text-gray-500 mb-1">Category</Label>
+          <Select>
+            <SelectTrigger className="w-45 h-10 rounded-lg bg-gray-100 border-none">
+              <SelectValue placeholder="All Categories" />
+            </SelectTrigger>
 
-    <Select>
-      <SelectTrigger className="w-45 h-10 rounded-lg bg-gray-100 border-none">
-        <SelectValue placeholder="All Categories" />
-      </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">All Categories</SelectItem>
 
-      <SelectContent>
-        <SelectItem value="all">All Categories</SelectItem>
+              {categories.map((cat) => (
+                <SelectItem key={cat} value={cat}>
+                  {cat}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
 
-        {categories.map((cat) => (
-          <SelectItem key={cat} value={cat}>
-            {cat}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  </div>
-
-  {/* Right: Count */}
-  <span className="text-lg font-semibold text-gray-500 whitespace-nowrap">
-    {transactions.length} transactions
-  </span>
-
-</div>
+        {/* Right: Count */}
+        <span className="text-lg font-semibold text-gray-500 whitespace-nowrap">
+          {transactions.length} transactions
+        </span>
+      </div>
 
       {/* Table */}
       <div className="border rounded-xl overflow-hidden">
         <Table>
           <TableHeader>
-            <TableRow className="text-lg" >
+            <TableRow className="text-lg">
               <TableHead>Title</TableHead>
               <TableHead>Category</TableHead>
               <TableHead>Type</TableHead>
@@ -108,7 +120,6 @@ export function TransactionTableUI() {
 
                 return (
                   <TableRow key={tx.id}>
-                    
                     {/* Title */}
                     <TableCell className="font-medium text-lg">
                       {tx.title}
@@ -153,11 +164,16 @@ export function TransactionTableUI() {
                     {/* Actions */}
                     <TableCell className="text-right">
                       <div className="flex justify-end gap-3">
-                        <Pencil className="h-6 w-6 cursor-pointer text-gray-600 hover:text-black" />
-                        <Trash2 className="h-6 w-6 cursor-pointer text-red-500 hover:text-red-700" onClick={() => handledelete(tx.id!)} />
+                        <Pencil
+                          className="h-6 w-6 cursor-pointer text-gray-600 hover:text-black"
+                          onClick={() => handleEdit(tx.id!)}
+                        />
+                        <Trash2
+                          className="h-6 w-6 cursor-pointer text-red-500 hover:text-red-700"
+                          onClick={() => handledelete(tx.id!)}
+                        />
                       </div>
                     </TableCell>
-
                   </TableRow>
                 );
               })
