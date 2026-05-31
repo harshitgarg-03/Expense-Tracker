c6efa71ef8c964b1a6bec0a3f6d7ff478f6099ad
Harshit Garg~ <gargh2120@gmail.com>
2026-05-31 17:28:35 +0530

    expenses tracker

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: "postgresql://postgres:1610@localhost:5432/test",
   },
 });
