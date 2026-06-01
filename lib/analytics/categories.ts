4b82d12966daa0677ca30da84467268c9be51e3c
Harshit Garg~ <gargh2120@gmail.com>
2026-06-01 21:46:57 +0530

    file fteues added

---
+++ lib/analytics/categories.ts
@@ -0,0 +1,3 @@
+function get_total_spents(){
+    
+}
--- module/home/components/dashboard/dashboardpage.tsx
+++ module/home/components/dashboard/dashboardpage.tsx
@@ -10,6 +10,7 @@ import AIInsights from "./ai-insights";
             
 export function DashboardPage() {
   const { balance, monthlyData, transactions, isLoading } = useTransaction();
+  console.log("total usetransaction data is " + balance + " " + monthlyData + " " + transactions + " " + isLoading );
   
   return (       
     <motion.div
