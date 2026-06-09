6375765fcaa8d577b89475b8745ade14f6646261
Harshit Garg~ <gargh2120@gmail.com>
2026-06-09 20:52:53 +0530

    sign in with google implement

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,3 +3,9 @@ export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: "http://localhost:3000"
 })
+
+const signIn = async (provider: "google" | "github") => {
+  const data = await authClient.signIn.social({
+    provider,
+  });
+};
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
+import { nextCookies } from "better-auth/next-js"
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
@@ -13,12 +14,20 @@ export const auth = betterAuth({
   },
 
   socialProviders: {
+
+    google: { 
+            prompt: "select_account",
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        },
+        
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
 
+  plugins: [nextCookies()],
   session: {
     expiresIn: 60 * 60 * 24 * 7,
   },
