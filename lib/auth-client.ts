f3a724d18079558e17fd2a7e560498273950eb60
Harshit Garg~ <gargh2120@gmail.com>
2026-06-11 08:37:51 +0530

    url changes

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://expense-tracker-orpin-nu-68.vercel.app"
 })
 
 const signIn = async (provider: "google" | "github") => {
