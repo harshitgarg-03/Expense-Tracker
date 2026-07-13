72bfd8d14227d15e74306961b58a6798e9933d5e
Harshit Garg~ <gargh2120@gmail.com>
2026-07-13 22:26:17 +0530

    adding routes

--- app/api/auth/.well-known/oauth-protected-resource/route.ts
+++ app/api/auth/.well-known/oauth-protected-resource/route.ts
@@ -1,4 +1,4 @@
 import { oAuthProtectedResourceMetadata } from "better-auth/plugins";
-import { auth } from "@/lib/auth";
+import { auth } from "../../../../../lib/auth";
 
 export const GET = oAuthProtectedResourceMetadata(auth);
