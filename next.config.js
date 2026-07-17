851a6f4e82e0d9bd4289592e4b54719f57e5822f
Harshit Garg~ <gargh2120@gmail.com>
2026-07-17 22:41:00 +0530

    adding next config js files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  async redirects() {
+    return [
+      { source: "/login", destination: "/signup", permanent: false },
+    ];
+  }
 };
 
 module.exports = nextConfig;
