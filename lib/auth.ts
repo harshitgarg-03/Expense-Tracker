a5938aedf242417d2294ca163fa57d723de0bdd4
Harshit Garg~ <gargh2120@gmail.com>
2026-07-23 20:09:29 +0530

    indentation

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import { nextCookies } from "better-auth/next-js";
 import { prisma } from "./prisma";
 import { jwt } from "better-auth/plugins";
 import { oauthProvider } from "@better-auth/oauth-provider";
-
+ 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
