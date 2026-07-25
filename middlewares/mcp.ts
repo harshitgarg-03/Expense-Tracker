03150b786e994b368626afaba7986dc6aa4d7558
Harshit Garg~ <gargh2120@gmail.com>
2026-07-25 20:49:19 +0530

    allowed origin urls

--- middlewares/mcp.ts
+++ middlewares/mcp.ts
@@ -4,6 +4,13 @@ import type { NextRequest } from "next/server";
 
 const ALLOWED_ORIGINS = [
   "http://localhost:6274", // MCP Inspector
+  "https://chatgpt.com", // CHATGPT url
+  "https://chat.openai.com", // CHATGPT url,
+  "https://claude.ai", // Claude url,
+  "https://gemini.google.com", // Gemini url,
+  "https://anthropic.com", // Anthropic url,
+  "https://gemini.google.com/share/", // Gemini url,
+  "https://claude.ai/api/v1/mcp", // Claude url,
 ];
 
 export function middleware(request: NextRequest) {
