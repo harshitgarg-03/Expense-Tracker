d9c5f9982be833586beb62b6c2be845f1dc7e278
Harshit Garg~ <gargh2120@gmail.com>
2026-07-26 14:42:39 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async redirects() {
-    return [
-      { source: "/login", destination: "/signup", permanent: false },
-    ];
-  }
 };
 
 module.exports = nextConfig;
